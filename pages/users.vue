0c5230528f2006acc836c3a1eb1f0cd59d1cfff0
AlinaDev252 <costachealina25@gmail.com>
2022-03-17 11:52:46 +0100

    Add a nested route

---
+++ pages/users.vue
@@ -0,0 +1,22 @@
+<template>
+   <div class="container">
+    <input type="text" v-model="userId">
+    <button @click="onLoadUser">Load User</button>
+    <NuxtChild />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      userId: '',
+    }
+  },
+  methods: {
+    onLoadUser() {
+      this.$router.push('/users/' + this.userId)
+    }
+  }
+}
+</script>
--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,21 +1,3 @@
 <template>
-  <div class="container">
-    <input type="text" v-model="userId">
-    <button @click="onLoadUser">Load User</button>
-  </div>
+  <p>Please enter am id and click the button</p>
 </template>
-
-<script>
-export default {
-  data() {
-    return {
-      userId: '',
-    }
-  },
-  methods: {
-    onLoadUser() {
-      this.$router.push('/users/' + this.userId)
-    }
-  }
-}
-</script>
